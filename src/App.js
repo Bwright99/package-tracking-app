142a30ba3385e9a422d3c3b3c7e75a69d8a666e0
Bernard Wright <68482257+Bwright99@users.noreply.github.com>
2023-09-26 18:56:56 -0400

    Initial Project Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [trackingNumber, setTrackingNumber] = useState('');
+  const [trackingResult, setTrackingResult] = useState('');
+
+  const trackPackage = async () => {
+
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Track Your Package</h1>
+      <input
+        type="text"
+        placeholder="Enter tracking number"
+        value={trackingNumber}
+        onChange={(e) => setTrackingNumber(e.target.value)}
+      />
+      <button onClick={trackPackage}>Track</button>
+      <div>{trackingResult}</div>
     </div>
   );
 }
