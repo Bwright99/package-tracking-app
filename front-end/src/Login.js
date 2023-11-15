ec4b04b641fdf28f0c4cb73ac6bed8c5aef57579
Bernard Wright <68482257+Bwright99@users.noreply.github.com>
2023-11-14 19:30:57 -0500

    N?

--- front-end/src/Login.js
+++ front-end/src/Login.js
@@ -1,86 +1,172 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-const Login = () => {
-  const [username, setUsername] = useState('')
-  const [password, setPassword] = useState('')
-  const [resetPasswordRequested, setResetPasswordRequested] = useState(false)
+const BASE_URL = 'http://localhost:2000';
 
-  const handleUsernameChange = (event) => {
-    setUsername(event.target.value)
-  }
+const api = {
+  signUp: async (newUsername, newPassword) => {
+    try {
+      const response = await fetch(`http://localhost:2000/signUp`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username: newUsername,
+          password: newPassword,
+        }),
+      });
 
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value)
-  }
+      if (!response.ok) {
+        throw new Error('Sign Up failed');
+      }
 
-  const handleLogin = () => {
-    let loginIdentifier = ''
-
-    const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
-    if (username.match(emailPattern)) {
-      loginIdentifier = 'email'
-    } else {
-      loginIdentifier = 'username'
+    } catch (error) {
+      console.error('Error during sign up:', error.message);
     }
+  },
 
-    const loginInfo =
-      loginIdentifier === 'email'
-        ? `Login with email: ${username} and password: ${password}`
-        : `Login with username: ${username} and password: ${password}`
+  signIn: async (username, password) => {
+    try {
+      const response = await fetch(`http://localhost:2000/signIn`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          username,
+          password,
+        }),
+      });
 
-    //navigateToHomePage();
-  }
+      if (!response.ok) {
+        throw new Error('Sign In failed');
+      }
 
-  const handleForgotPassword = () => {
-    console.log('Sending password reset link to:', username)
-    setResetPasswordRequested(true)
-  }
+    } catch (error) {
+      console.error('Error during sign in:', error.message);
+    }
+  },
+};
+
+const SignUp = () => {
+  const [newUsername, setNewUsername] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+
+  const handleNewUsernameChange = (event) => {
+    setNewUsername(event.target.value);
+  };
+
+  const handleNewPasswordChange = (event) => {
+    setNewPassword(event.target.value);
+  };
+
+  const handleSignUp = async () => {
+    await api.signUp(newUsername, newPassword);
+  };
 
   return (
-    <div style={styles.container}>
-      <h2 style={styles.title}>Login</h2>
+    <div style={{ ...styles.container, ...styles.signUpContainer }}>
+      <h2 style={styles.title}>Sign Up</h2>
       <div style={styles.inputContainer}>
         <input
           type='text'
-          placeholder='Username'
-          value={username}
-          onChange={handleUsernameChange}
+          placeholder='New Username'
+          value={newUsername}
+          onChange={handleNewUsernameChange}
           style={styles.input}
         />
       </div>
       <div style={styles.inputContainer}>
         <input
           type='password'
-          placeholder='Password'
-          value={password}
-          onChange={handlePasswordChange}
+          placeholder='New Password'
+          value={newPassword}
+          onChange={handleNewPasswordChange}
           style={styles.input}
         />
       </div>
-      <button onClick={handleLogin} style={styles.button}>
-        <Link to='/Home'>Login</Link>
+      <button onClick={handleSignUp} style={styles.button}>
+        Sign Up
       </button>
-      <p style={styles.forgotPassword} onClick={handleForgotPassword}>
-        Forgot Password?
-      </p>
-      {resetPasswordRequested && (
-        <p style={styles.resetPasswordMessage}>
-          Password reset link sent to {username}'s email.
-        </p>
-      )}
-      <p style={styles.signupText}>
-        New Here? <span style={styles.signupLink}>Sign Up</span>
-      </p>
     </div>
-  )
-}
+  );
+};
+
+const Login = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [resetPasswordRequested, setResetPasswordRequested] = useState(false);
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = async () => {
+    await api.signIn(username, password);
+  };
+
+  const handleForgotPassword = () => {
+    console.log('Sending password reset link to:', username);
+    setResetPasswordRequested(true);
+  };
+
+  return (
+    <div style={styles.flexContainer}>
+      {/* Login component */}
+      <div style={{ ...styles.container, ...styles.loginContainer }}>
+        <h2 style={styles.title}>Login</h2>
+        <div style={styles.inputContainer}>
+          <input
+            type='text'
+            placeholder='Username'
+            value={username}
+            onChange={handleUsernameChange}
+            style={styles.input}
+          />
+        </div>
+        <div style={styles.inputContainer}>
+          <input
+            type='password'
+            placeholder='Password'
+            value={password}
+            onChange={handlePasswordChange}
+            style={styles.input}
+          />
+        </div>
+        <button onClick={handleLogin} style={styles.button}>
+          <Link to='/Home'>Login</Link>
+        </button>
+        <p style={styles.forgotPassword} onClick={handleForgotPassword}>
+          Forgot Password?
+        </p>
+        {resetPasswordRequested && (
+          <p style={styles.resetPasswordMessage}>
+            Password reset link sent to {username}'s email.
+          </p>
+        )}
+      </div>
+
+      {/* Sign-up component */}
+      <SignUp />
+    </div>
+  );
+};
 
 const styles = {
-  container: {
-    maxWidth: '400px',
+  flexContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '80%',
     margin: 'auto',
     marginTop: '100px',
+  },
+  container: {
+    width: '48%',
     padding: '40px',
     border: '1px solid #ccc',
     borderRadius: '10px',
@@ -95,7 +181,7 @@ const styles = {
   },
   inputContainer: {
     marginBottom: '20px',
-    width: '80%',
+    width: '100%',
   },
   input: {
     width: '100%',
@@ -105,7 +191,7 @@ const styles = {
     marginBottom: '10px',
   },
   button: {
-    width: '80%',
+    width: '100%',
     padding: '15px',
     borderRadius: '5px',
     border: 'none',
@@ -124,14 +210,12 @@ const styles = {
     color: 'green',
     marginBottom: '10px',
   },
-  signupText: {
-    marginBottom: '10px',
+  signUpContainer: {
+    width: '48%',
   },
-  signupLink: {
-    color: '#007BFF',
-    textDecoration: 'underline',
-    cursor: 'pointer',
+  loginContainer: {
+    width: '48%',
   },
-}
+};
 
-export default Login
+export default Login;
